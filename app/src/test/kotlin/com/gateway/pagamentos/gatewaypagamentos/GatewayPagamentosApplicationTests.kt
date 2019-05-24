8afe88c5031ae03ab1146dc7f2c249786cdede43
brunoroger <bschiochet@gmail.com>
2019-05-24 08:48:51 -0500

    finish implementing tests spring

--- app/src/test/kotlin/com/gateway/pagamentos/gatewaypagamentos/GatewayPagamentosApplicationTests.kt
+++ app/src/test/kotlin/com/gateway/pagamentos/gatewaypagamentos/GatewayPagamentosApplicationTests.kt
@@ -1,16 +1,22 @@
 package com.gateway.pagamentos.gatewaypagamentos
 
-import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.extension.ExtendWith
+import org.junit.Test
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.context.junit.jupiter.SpringExtension
+import org.springframework.test.context.junit4.SpringRunner
+import org.junit.runner.RunWith
+import org.springframework.boot.SpringBootConfiguration
+import org.springframework.test.context.web.WebAppConfiguration
 
-@ExtendWith(SpringExtension::class)
-@SpringBootTest
+
+@RunWith(SpringRunner::class)
+//@SpringBootConfiguration
+//@WebAppConfiguration
 class GatewayPagamentosApplicationTests {
 
 	@Test
 	fun contextLoads() {
 	}
+
 
 }
--- app/src/test/kotlin/com/gateway/pagamentos/gatewaypagamentos/controller/CreditCardControllerTest.kt
+++ app/src/test/kotlin/com/gateway/pagamentos/gatewaypagamentos/controller/CreditCardControllerTest.kt
@@ -1,29 +1,63 @@
 package com.gateway.pagamentos.gatewaypagamentos.controller
 
-import com.gateway.pagamentos.gateway.entity.CreditCard
-import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.extension.ExtendWith
+import com.gateway.pagamentos.gateway.controller.CreditCardController
+import org.json.JSONObject
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.MockitoAnnotations
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.http.MediaType
-import org.springframework.test.context.junit.jupiter.SpringExtension
+import org.springframework.test.context.junit4.SpringRunner
 import org.springframework.test.web.servlet.MockMvc
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import org.springframework.test.web.servlet.setup.MockMvcBuilders
 
-@ExtendWith(SpringExtension::class)
-@WebMvcTest(CreditCard::class)
-internal class CreditCardControllerTest {
+
+@RunWith(SpringRunner::class)
+class CreditCardControllerTest {
+
+    private var mockMvc: MockMvc? = null
 
     @Autowired
-    private lateinit var mockMvc: MockMvc
+    private val creditCardController: CreditCardController? = CreditCardController()
 
-    @Throws(Exception::class)
+
+    @Before
+    fun setUp() {
+        MockitoAnnotations.initMocks(this);
+        this.mockMvc = MockMvcBuilders.standaloneSetup(creditCardController).build()
+    }
+
     @Test
-    fun testGetOne(){
-        mockMvc.perform(get("/credit_card/1"))
-                .andExpect(status().isOk)
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8));
+    @Throws(Exception::class)
+    fun findOneTest() {
+        this.mockMvc!!.perform(MockMvcRequestBuilders.get("/credit_card/1"))
+            .andExpect(status().isOk)
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun addCreditSuccess() {
+        val credit = mapOf(
+            "clientId" to 1,
+            "statementDescriptor" to "JOSE SILVA",
+            "number" to "1234567890987",
+            "holderName" to "JOSE SILVA",
+            "holderDocument" to "10188607030",
+            "expirationDate" to "0420",
+            "cvv" to "187"
+        )
+
+        this.mockMvc!!.perform(
+            MockMvcRequestBuilders.post("/credit_card")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(JSONObject(credit).toString())
+        )
+            .andExpect(status().isCreated)
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
     }
 }
